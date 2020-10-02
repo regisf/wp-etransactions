bf014a265e3d0ff2cf8ce8d38542a4602aa90a3e
regisf <regisfloret@protonmail.com>
2020-10-02 09:57:15 +0200

    Use lambda function for hook registration

--- wp-etransaction/admin/hooks/register.php
+++ wp-etransaction/admin/hooks/register.php
@@ -1,10 +1,11 @@
 <?php
 
 register_activation_hook(__FILE__, function () {
+    // Install database
     $productDb = new ProductsDb();
     $productDb->install();
 });
 
-register_deactivation_hook(__FILE__, function() {
+register_deactivation_hook(__FILE__, function () {
     //
 });
