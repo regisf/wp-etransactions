9c961ee32e1d9daba7dc8b9c85d66be1463d4b39
regisf <regisfloret@protonmail.com>
2020-10-02 15:58:53 +0200

    Filter to get the validation page address

--- wp-etransaction/admin/hooks/filters.php
+++ wp-etransaction/admin/hooks/filters.php
@@ -8,3 +8,8 @@ add_filter('is_nonce_exists', function ($nonce) {
 add_filter('nonce_input', function () {
     return '<input type="hidden" name="_wpnonce" value="' . wp_create_nonce(NonceName) . '" />';
 });
+
+add_filter('etransaction_get_order_address', function ($id) {
+    $options = get_option('etransactions_options');
+    return $options['validation_page'] . '?product=' . $id;
+});
